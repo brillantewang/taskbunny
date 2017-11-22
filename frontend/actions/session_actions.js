b300674d0a6d73d1d61d5f5450730c4fdb9a34d4
Brillante Wang <brillante.wang@gmail.com>
2017-11-21 21:46:23 -0800

    create session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,40 @@
+import * as sessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+})
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+})
+
+//thunk
+
+export login = user => dispatch => (
+  sessionAPIUtil.login(user)
+    .then(
+      currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveErrors(errors))
+    )
+)
+
+export logout = () => dispatch => (
+  sessionAPIUtil.logout()
+    .then(
+      res => dispatch(receiveCurrentUser(null)),
+      errors => dispatch(receiveErrors(errors))
+    )
+)
+
+export signup = user => dispatch => (
+  sessionAPIUtil.signup(user)
+    .then(
+      currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveErrors(errors))
+    )
+)
