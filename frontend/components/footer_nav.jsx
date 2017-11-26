02c666dba93bc540a3bcffe5a3211a8afa019151
Brillante Wang <brillante.wang@gmail.com>
2017-11-25 16:29:30 -0800

    Delete font weight 300 from many places in scss files by putting it on body

--- frontend/components/footer_nav.jsx
+++ frontend/components/footer_nav.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 
 export const FooterNav = () => {
   return (
-    <h1>FooterNav</h1>
+    <div className="footer-nav">
+      <nav>
+        <strong>Discover</strong>
+        <a>Become a Tasker</a>
+        <a>The TaskWombat Elite</a>
+        <a>Buy a Gift Card</a>
+        <a>TaskWombat for Good</a>
+        <a>Help</a>
+      </nav>
+      <nav>
+        <strong>Company</strong>
+        <a>About Us</a>
+        <a>Careers</a>
+        <a>Press</a>
+        <a>Blog</a>
+        <a>Terms & Privacy</a>
+      </nav>
+      <div>
+        <strong>Download our app</strong>
+        <p>Tackle your to-do list whenever you are with our mobile app.</p>
+        <button><img/></button>
+        <button><img/></button>
+      </div>
+    </div>
   )
 }
