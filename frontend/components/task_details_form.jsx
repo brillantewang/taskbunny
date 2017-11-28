f1114ed77c3325354530ecae7faaa224d94d24d5
Brillante Wang <brillante.wang@gmail.com>
2017-11-27 21:57:16 -0800

    Add task description details subform

--- frontend/components/task_details_form.jsx
+++ frontend/components/task_details_form.jsx
@@ -38,32 +38,48 @@ class TaskDetailsForm extends React.Component {
           <div className="vehicle-details-form-inputs">
             <span className="vehicle-option">
               <input
+                id="no-vehicle"
                 type="radio"
                 value=""
                 checked={this.props.state.vehicle_requirements === ""}
                 onChange={this.props.handleChange("vehicle_requirements")}
               />
-              <label><strong>Not needed</strong> for task</label>
+              <label for="no-vehicle"><strong>Not needed</strong> for task</label>
             </span>
             <span className="vehicle-option">
               <input
+                id="car"
                 type="radio"
                 value="A car is needed"
                 checked={this.props.state.vehicle_requirements === "A car is needed"}
                 onChange={this.props.handleChange("vehicle_requirements")}
               />
-              <label>Task requires a <strong>car</strong></label>
+              <label for="car">Task requires a <strong>car</strong></label>
             </span>
             <span className="vehicle-option">
               <input
+                id="truck"
                 type="radio"
                 value="A truck is needed"
                 checked={this.props.state.vehicle_requirements === "A truck is needed"}
                 onChange={this.props.handleChange("vehicle_requirements")}
               />
-              <label>Task requires a <strong>truck</strong></label>
+              <label for="truck">Task requires a <strong>truck</strong></label>
             </span>
           </div>
+          <div className="save-button-container">
+            <button className="btn-green">Continue</button>
+          </div>
+        </form>
+        <form onSubmit={this.props.handleSubmit} className="task-details-subform description-details-form">
+          <strong>TELL US ABOUT YOUR TASK</strong>
+          <p>Tell us what you need done, plus any requirements or questions that you may have. You can edit this later.</p>
+          <textarea value={this.props.state.description}
+            onChange={this.props.handleChange('description')}
+            className="description-details-form-textarea Description"
+            placeholder="EXAMPLE: I rented a moving van, but need help moving my stuff in and out of it. I have: queen bed with frame, medium couch, loveseat, entertainment center, large TV, armchair, 2 bookcases, dining room table with 4 chairs, desk and chair, and about 50 boxes.">
+          </textarea>
+          {this.props.handleErrorInput('Description')}
           <div className="save-button-container">
             <button className="btn-green">Continue</button>
           </div>
