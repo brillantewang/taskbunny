c1932cd3494d2fc09851af5742e30b73de51037d
Brillante Wang <brillante.wang@gmail.com>
2017-11-28 15:59:04 -0800

    Add and style select field in pick a tasker form

--- frontend/components/pick_tasker_form.jsx
+++ frontend/components/pick_tasker_form.jsx
@@ -10,7 +10,26 @@ class PickTaskerForm extends React.Component {
       <div className="pick-tasker-form task-form-subform">
         <h2>Pick a Tasker</h2>
         <h3>After booking, you can chat with your Tasker, agree on an exact time, or go over any requirements or questions, if necessary.</h3>
-
+        <div className="pick-tasker-form-content">
+          <div className="date-time-picker">
+            <strong>SORTED BY:</strong>
+            <select>
+              <option value="Recommended">Recommended</option>
+              <option value="Price (Lowest To Highest)">Price (Lowest To Highest)</option>
+              <option value="Price (Highest To Lowest)">Price (Highest To Lowest)</option>
+              <option value="Highest Rating">Highest Rating</option>
+              <option value="Most Reviews">Most Reviews</option>
+              <option value="Number Of Tasks">Number Of Tasks</option>
+            </select>
+            <strong>
+              <i class="fa fa-clock-o" aria-hidden="true"></i>
+              TASK DATE & TIME:
+            </strong>
+          </div>
+          <div className="reviews">
+            Review
+          </div>
+        </div>
       </div>
     )
   }
