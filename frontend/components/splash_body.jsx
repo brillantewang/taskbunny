1d33983bdc95cfff273681d1c3fe0462573aea0b
Brillante Wang <brillante.wang@gmail.com>
2018-01-28 19:46:26 -0800

    add createTask callback to onclick for all splash body images

--- frontend/components/splash_body.jsx
+++ frontend/components/splash_body.jsx
@@ -57,27 +57,32 @@ class SplashBody extends React.Component {
             onClick={() => this.handleImageClick("Minor Repairs")}>
             <img src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_2973,w_3877/v1511571163/splash-body-img1_gnn4se.jpg"/>
           </a>
-          <Link to="/login" className="splash-body-top-item-2">
+          <a
+            className="splash-body-top-item-2"
+            onClick={() => this.handleImageClick("Minor Repairs")}>
             <h3>Tackle those home projects & repairs you've been putting off</h3>
             <button className="btn-green">Book Handyman</button>
-          </Link>
+          </a>
         </div>
         <div className="splash-body-bottom">
-          <Link to="/login">
+          <a
+            onClick={() => this.handleImageClick("Mounting")}>
             <img className="splash-body-img" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_501,w_565,x_0,y_60/v1511572825/splash_body_img2_q8ekt9.jpg"/>
             <h3>Mount a TV or mirror</h3>
             <button className="btn-green">Book Mounting</button>
-          </Link>
-          <Link to="/login">
+          </a>
+          <a
+            onClick={() => this.handleImageClick("Assembly")}>
             <img className="splash-body-img" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_469,w_573,x_0,y_26/v1511572825/splash_body_img3_y0fp9x.jpg"/>
             <h3>Put together furniture</h3>
             <button className="btn-green">Book Assembly</button>
-          </Link>
-          <Link to="/login">
+          </a>
+          <a
+            onClick={() => this.handleImageClick("Help Moving")}>
             <img className="splash-body-img" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_381,w_445,x_859,y_400/v1511572825/splash_body_img4_mrpzwp.jpg"/>
             <h3>Lift & shift heavy items</h3>
             <button className="btn-green">Book Help Moving</button>
-          </Link>
+          </a>
         </div>
       </div>
     )
