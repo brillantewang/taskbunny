10403fdfa44ad9fa5862f75e1102797b483f909a
Brillante Wang <brillante.wang@gmail.com>
2018-02-05 16:32:09 -0800

    remove all comments from dashboard

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -8,7 +8,6 @@ import Modal from 'react-modal';
 import todaysDateString from "../util/todays_date_util";
 import { FooterNav } from './footer_nav';
 import { ClipLoader } from 'react-spinners';
-// import { SplashBody } from './splash_body';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -43,16 +42,13 @@ class Dashboard extends React.Component {
 
   componentDidMount() {
     this.props.fetchAllTasks();
-    // this.props.fetchAllUsers();
   }
 
   handleClick(e) {
     const nodeClasses= e.target.classList;
-    // console.log(nodeClasses);
     const taskCategoriesClass = classNames({
       'hidden': !nodeClasses.contains("search")
     })
-    // console.log(taskCategoriesClass);
 
     document.getElementById('task-categories').className = taskCategoriesClass;
   }
@@ -90,175 +86,153 @@ class Dashboard extends React.Component {
   }
 
   render() {
-    // console.log('dashboard rendering');
-    // console.log(this.props.currentUser);
-    // console.log(this.props.currentUserTasks, 'current user tasks');
-
-    // if (this.props.currentUserTasks.length > 0) {
-      return (
-        <div className="dashboard" onClick={this.handleClick}>
-          <Route path="/dashboard" component={NavBarContainer}/>
-          <div className="dashboard-head">
-            <div className="user-greeting">
-              <img className="user-profile-img" src={this.props.currentUser.image_url}/>
-              <h2>Welcome back, {this.props.currentUser.first_name}.</h2>
-            </div>
-            <TaskSearchWithRouter/>
+    return (
+      <div className="dashboard" onClick={this.handleClick}>
+        <Route path="/dashboard" component={NavBarContainer}/>
+        <div className="dashboard-head">
+          <div className="user-greeting">
+            <img className="user-profile-img" src={this.props.currentUser.image_url}/>
+            <h2>Welcome back, {this.props.currentUser.first_name}.</h2>
           </div>
-          <div className="dashboard-body">
-            <div className="dashboard-body-header-container">
-              <h3 className="dashboard-body-header">Tasks</h3>
-            </div>
-            {this.props.currentUserTasks.map(task => {
-              // console.log(task.id, 'task id in dashboard');
-              // console.log(task, 'task in dashboard');
-              // console.log(task.tasker, 'tasker');
-              // console.log(task.tasker.image_url, 'tasker image url');
-              // let tasker = this.props.getUser(task.tasker_id);
-              return (
-                <div key={task.id} id={task.id} className="dashboard-task">
-                  <div className="dashboard-task-header">
-                    <h2 className="dashboard-task-header-title">{task.task_type}</h2>
-                    <div className="tasker-cancel-section">
-                      <img className="tasker-profile-img" src={task.tasker.image_url}/>
-                      {!task.complete ? <a onClick={() => this.handleClickCancel(task.id)} className="cancel-task-link">Cancel Task</a> : ""}
-                    </div>
-                  </div>
-                  <div className="dashboard-task-status">
-                    <strong>{task.complete ? "Your task is complete." : `Your task is booked with ${task.tasker.first_name} ${task.tasker.last_name[0]}.`}</strong>
-                  </div>
-                  <div className="date-time-section">
-                    <div className="date-section">
-                      <strong className="date-time-section-day">{task.date.split('-')[2]}</strong>
-                      <strong className="date-time-section-month">{this.getMonth(task.date.split('-')[1])}</strong>
-                    </div>
-                    <div className="time-section">
-                      <strong className="date-time-section-time">{task.task_time}</strong>
-                    </div>
-                  </div>
-                  <div className="dashboard-task-body hidden">
-                    <div className="location-tasker-price-section">
-                      <div className="dashboard-task-body-location">
-                        <strong className="dashboard-task-mini-header">Location</strong>
-                        <strong className="location-address"><i className="fa fa-map-marker" aria-hidden="true"></i> {task.location}</strong>
-                      </div>
-                      <div className="dashboard-task-body-tasker">
-                        <strong className="dashboard-task-mini-header">Tasker</strong>
-                        <strong className="dashboard-task-body-tasker-name">{task.tasker.first_name} {task.tasker.last_name[0]}.</strong>
-                      </div>
-                      <div className="dashboard-task-body-price">
-                        <strong className="dashboard-task-mini-header">Price</strong>
-                        <strong className="dashboard-task-body-tasker-name">${task.tasker.price_per_hour}/hr</strong>
-                      </div>
-                    </div>
-                    <div className="dashboard-task-body-description">
-                      <strong className="dashboard-task-mini-header">Description</strong>
-                      <p className="dashboard-task-body-description-text">{task.description}</p>
-                    </div>
-                    <div className="small-border"></div>
-                    <div className="dashboard-task-body-vehicle-requirements">
-                      <strong>Vehicle Requirements:</strong>
-                      <strong className="dashboard-task-body-vehicle-requirements-text">{task.vehicle_requirements}</strong>
-                    </div>
-                  </div>
-                  <div className="toggle-details">
-                    <a onClick={() => this.collapse(task.id)}>
-                      <ToggleText/>
-                    </a>
-                  </div>
-                </div>
-              )
-            })}
-            {/* <SplashBody/> */}
-          </div>
-          <div className="dashboard-bottom">
-            <h2 className="dashboard-bottom-header">How We Can Help</h2>
-            <div className="dashboard-bottom-row">
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Mounting")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_501,w_565,x_0,y_60/v1511572825/splash_body_img2_q8ekt9.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Mounting</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Mount a TV or mirror</h3>
-              </a>
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Assembly")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_469,w_573,x_0,y_26/v1511572825/splash_body_img3_y0fp9x.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Assembly</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Put together furniture</h3>
-              </a>
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Help Moving")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_381,w_445,x_859,y_400/v1511572825/splash_body_img4_mrpzwp.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Help Moving</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Lift & shift heavy items</h3>
-              </a>
-            </div>
-            <div className="dashboard-bottom-row">
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Cleaning")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_3648,w_4163/v1511562973/general_cleaning_wnm6r4.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Cleaning</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Get a sparkling home</h3>
-              </a>
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Yard work")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_420,w_455/v1511562988/yard_work_byqags.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Yard Work</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Maintain outside spaces</h3>
-              </a>
-              <a
-                className="dashboard-bottom-image-link"
-                onClick={() => this.handleImageClick("Minor Repairs")}>
-                <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/v1511562978/minor_repairs_bozruc.jpg"/>
-                <div className="dashboard-bottom-image-tag">
-                  <strong>Minor Repairs</strong>
-                </div>
-                <h3 className="dashboard-bottom-image-text">Make home improvements</h3>
-              </a>
-            </div>
-          </div>
-          <FooterNav/>
-          <Modal
-            className="modal-container"
-            overlayClassName="modal-container-overlay"
-            onRequestClose={this.closeModal.bind(this)}
-            isOpen={this.state.modalIsOpen}
-            >
-            <div className="cancel-modal">
-              <h3 className="cancel-modal-header">Cancel Task</h3>
-              <p className="cancel-modal-text">Are you sure you want to cancel this task?</p>
-              <button onClick={this.handleConfirmCancel.bind(this)} className="btn-green cancel-modal-btn">Confirm</button>
-              <a className="cancel-modal-link" onClick={this.closeModal.bind(this)}>No, I don't want to cancel</a>
-            </div>
-          </Modal>
+          <TaskSearchWithRouter/>
         </div>
-      )
-    // } else {
-    //   return (
-    //     <div>
-    //       <Route path="/dashboard" component={NavBarContainer}/>
-    //       <div className="loader-icon">
-    //         <ClipLoader/>
-    //       </div>
-    //     </div>
-    //   )
-    // }
-
+        <div className="dashboard-body">
+          <div className="dashboard-body-header-container">
+            <h3 className="dashboard-body-header">Tasks</h3>
+          </div>
+          {this.props.currentUserTasks.map(task => {
+            return (
+              <div key={task.id} id={task.id} className="dashboard-task">
+                <div className="dashboard-task-header">
+                  <h2 className="dashboard-task-header-title">{task.task_type}</h2>
+                  <div className="tasker-cancel-section">
+                    <img className="tasker-profile-img" src={task.tasker.image_url}/>
+                    {!task.complete ? <a onClick={() => this.handleClickCancel(task.id)} className="cancel-task-link">Cancel Task</a> : ""}
+                  </div>
+                </div>
+                <div className="dashboard-task-status">
+                  <strong>{task.complete ? "Your task is complete." : `Your task is booked with ${task.tasker.first_name} ${task.tasker.last_name[0]}.`}</strong>
+                </div>
+                <div className="date-time-section">
+                  <div className="date-section">
+                    <strong className="date-time-section-day">{task.date.split('-')[2]}</strong>
+                    <strong className="date-time-section-month">{this.getMonth(task.date.split('-')[1])}</strong>
+                  </div>
+                  <div className="time-section">
+                    <strong className="date-time-section-time">{task.task_time}</strong>
+                  </div>
+                </div>
+                <div className="dashboard-task-body hidden">
+                  <div className="location-tasker-price-section">
+                    <div className="dashboard-task-body-location">
+                      <strong className="dashboard-task-mini-header">Location</strong>
+                      <strong className="location-address"><i className="fa fa-map-marker" aria-hidden="true"></i> {task.location}</strong>
+                    </div>
+                    <div className="dashboard-task-body-tasker">
+                      <strong className="dashboard-task-mini-header">Tasker</strong>
+                      <strong className="dashboard-task-body-tasker-name">{task.tasker.first_name} {task.tasker.last_name[0]}.</strong>
+                    </div>
+                    <div className="dashboard-task-body-price">
+                      <strong className="dashboard-task-mini-header">Price</strong>
+                      <strong className="dashboard-task-body-tasker-name">${task.tasker.price_per_hour}/hr</strong>
+                    </div>
+                  </div>
+                  <div className="dashboard-task-body-description">
+                    <strong className="dashboard-task-mini-header">Description</strong>
+                    <p className="dashboard-task-body-description-text">{task.description}</p>
+                  </div>
+                  <div className="small-border"></div>
+                  <div className="dashboard-task-body-vehicle-requirements">
+                    <strong>Vehicle Requirements:</strong>
+                    <strong className="dashboard-task-body-vehicle-requirements-text">{task.vehicle_requirements}</strong>
+                  </div>
+                </div>
+                <div className="toggle-details">
+                  <a onClick={() => this.collapse(task.id)}>
+                    <ToggleText/>
+                  </a>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="dashboard-bottom">
+          <h2 className="dashboard-bottom-header">How We Can Help</h2>
+          <div className="dashboard-bottom-row">
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Mounting")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_501,w_565,x_0,y_60/v1511572825/splash_body_img2_q8ekt9.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Mounting</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Mount a TV or mirror</h3>
+            </a>
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Assembly")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_469,w_573,x_0,y_26/v1511572825/splash_body_img3_y0fp9x.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Assembly</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Put together furniture</h3>
+            </a>
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Help Moving")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_381,w_445,x_859,y_400/v1511572825/splash_body_img4_mrpzwp.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Help Moving</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Lift & shift heavy items</h3>
+            </a>
+          </div>
+          <div className="dashboard-bottom-row">
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Cleaning")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_3648,w_4163/v1511562973/general_cleaning_wnm6r4.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Cleaning</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Get a sparkling home</h3>
+            </a>
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Yard work")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/c_crop,h_420,w_455/v1511562988/yard_work_byqags.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Yard Work</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Maintain outside spaces</h3>
+            </a>
+            <a
+              className="dashboard-bottom-image-link"
+              onClick={() => this.handleImageClick("Minor Repairs")}>
+              <img className="dashboard-bottom-image" src="https://res.cloudinary.com/dezmnl5mf/image/upload/v1511562978/minor_repairs_bozruc.jpg"/>
+              <div className="dashboard-bottom-image-tag">
+                <strong>Minor Repairs</strong>
+              </div>
+              <h3 className="dashboard-bottom-image-text">Make home improvements</h3>
+            </a>
+          </div>
+        </div>
+        <FooterNav/>
+        <Modal
+          className="modal-container"
+          overlayClassName="modal-container-overlay"
+          onRequestClose={this.closeModal.bind(this)}
+          isOpen={this.state.modalIsOpen}
+          >
+          <div className="cancel-modal">
+            <h3 className="cancel-modal-header">Cancel Task</h3>
+            <p className="cancel-modal-text">Are you sure you want to cancel this task?</p>
+            <button onClick={this.handleConfirmCancel.bind(this)} className="btn-green cancel-modal-btn">Confirm</button>
+            <a className="cancel-modal-link" onClick={this.closeModal.bind(this)}>No, I don't want to cancel</a>
+          </div>
+        </Modal>
+      </div>
+    )
   }
 }
 
