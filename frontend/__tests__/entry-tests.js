b457661266d6f403e4f7db048a93202c614804cd
Brillante Wang <brillante.wang@gmail.com>
2018-04-05 15:50:29 -0700

    add configure store jest test

---
+++ frontend/__tests__/entry-tests.js
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom';
+import configureStore from '../store/store';
+
+jest.mock('../store/store', () => jest.fn(() => ({ storeKey: 'storeValue' }) ));
+
+describe('entry', () => {
+  let Entry, ReactDOM, Root;
+
+  beforeAll(() => {
+    document.addEventListener = jest.fn();
+    document.getElementById = jest.fn(id => id);
+
+    ReactDOM = require('react-dom');
+    ReactDOM.render = jest.fn();
+
+    Root = require('../components/root');
+    Entry = require('../taskwombat');
+
+    document.addEventListener.mock.calls[0][1]();
+  });
+
+  it('invokes the configureStore function', () => {
+    expect(configureStore).toBeCalled();
+  })
+})
